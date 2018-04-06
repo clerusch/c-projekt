5c882c785cdab2a67da8a124d2a4416fb1aa6802
David Reuschenberg <davidreuschenberg@web.de>
2018-04-06 12:24:48 +0200

    a

--- termjump.c
+++ termjump.c
@@ -1,3 +1,7 @@
+//Basic Terminal-Jump&Run
+//Authors: Theresa Johanni, Clemens Schumann, David Reuschenberg
+//After a template by Leonard König
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -5,16 +9,42 @@
 #include <time.h>
 #include <unistd.h>
 #include <ctype.h>
-
 #include <termbox.h>
-
 
 #define GAME_X 79
 #define GAME_Y 39
 #define LOG_FILENAME "/tmp/spacei.log"
-#define MONSTER_Y 4
 
 int x = 0, y = 0, z = 0, steigen = 1, neu=0, answer;
+
+struct tb_event input, leave;
+clock_t timeanf;
+time_t t1, t2;
+double timediff, random, counter1 = 0, counter2 = 0;
+   
+static unsigned int player_x = GAME_X/2;
+static unsigned int player_y = GAME_Y - 1;
+
+static bool obst[GAME_Y][GAME_X]; 
+
+static bool playerpos[GAME_Y][GAME_X]; 
+
+static const struct tb_cell empty = { 
+	.ch = ' ', 
+	.fg = TB_DEFAULT, 
+	.bg = TB_DEFAULT, 
+};
+
+static const struct tb_cell obs = {
+	.ch = '<',
+	.fg = TB_DEFAULT,
+	.bg = TB_RED,
+};
+static const struct tb_cell player = {
+	.ch = 'J',
+	.fg = TB_BLUE,
+	.bg = TB_GREEN,
+};
 
 // Integer to ASCII Funktion
 int my_itoa (int in, char **out_str)                                                     
@@ -37,35 +67,11 @@ int my_itoa (int in, char **out_str)
 
     return 0;                                                                        
 }                   
-    
-static unsigned int player_x = GAME_X/2;
-static unsigned int player_y = GAME_Y - 1;
 
-static bool obst[GAME_Y][GAME_X]; 
-
-static bool playerpos[GAME_Y][GAME_X]; 
-
-static const struct tb_cell empty = { 
-	.ch = ' ', 
-	.fg = TB_DEFAULT, 
-	.bg = TB_DEFAULT, 
-};
-
-static const struct tb_cell obs = {
-	.ch = '*',
-	.fg = TB_DEFAULT,
-	.bg = TB_YELLOW,
-};
-static const struct tb_cell player = {
-	.ch = '^',
-	.fg = TB_DEFAULT,
-	.bg = TB_GREEN,
-};
-
+//Function to print text via tb_cells
 static void tb_print (int posx, int posy, char *text)
 {
-
-	static struct tb_cell cellarray[GAME_X];
+	static struct tb_cell cellarray[GAME_X+100];
 
 	for (int i = 0; text[i] != '\0'; i++)
 	{
@@ -78,11 +84,15 @@ static void tb_print (int posx, int posy, char *text)
 	}
 }
 
+//let obstacles move from right to left
 static void update_obst(void)
 {
 	for (size_t y = 0; y < GAME_Y; y++) {
-		tb_put_cell(0, 38, &empty);
-		obst[38][0] = false;
+		tb_put_cell(0, GAME_Y - 1, &empty);
+		obst[GAME_Y - 1][0] = false;
+		tb_put_cell(0, GAME_Y - 2, &empty);
+		obst[GAME_Y - 2][0] = false;
+
 	}
 	for (size_t y = 1; y < sizeof(obst)/sizeof(obst[0]); y++) {
 		for (size_t x = 1; x < sizeof(obst[0]); x++) {
@@ -95,6 +105,8 @@ static void update_obst(void)
 		}
 	}
 }
+
+//let player move upwards 5 tiles after pressing spacekey
 int update_jump(void)
 {
 
@@ -123,47 +135,54 @@ int update_jump(void)
 			else return 1;
 }
 
+//let player move down again to ground level
 int update_land(void)
 {
-
-			if (z < 12) 
+	if (z < 12) 
+	{
+		for (x = GAME_X-1; x > 0; x--)
+		{
+			for (y = GAME_Y-2; y >= 0; y--)
 			{
-					for (x = GAME_X-1; x > 0; x--) {
-						for (y = GAME_Y-2; y >= 0; y--) {
-							if (playerpos[y][x])
-							{
-								playerpos[y][x] = false;
-								playerpos[y+1][x] = true;	
-								tb_put_cell(x, y, &empty);
-								tb_put_cell(x, y+1, &player);
-							}
-						}
-					}
-					z++;
-				
+				if (playerpos[y][x])
+				{
+					playerpos[y][x] = false;
+					playerpos[y+1][x] = true;	
+					tb_put_cell(x, y, &empty);
+					tb_put_cell(x, y+1, &player);
+				}
 			}
-			if (z == 12)
-			{
-			z = 0;	
-                        return 1;
-			}
-		       	else {
-                            return 0;}
+		}
+		z++;
+	}
+	if (z == 12)
+	{
+		z = 0;	
+                return 1;
+	}
+       	else {
+	        return 0;
+	}
 }
 
+
+// Initialize board
 static void re_init (void)
 {
-	// Initialize board
-	//   Player
-	for (x = 0; x < GAME_X; x++)
-		for (y = 0; y < GAME_Y; y++) {
+	//  Set player and obstacle arrays false false again
+	for (x = 0; x < GAME_X; x++) 
+	{
+		for (y = 0; y < GAME_Y; y++) 
+		{
 			playerpos[y][x] = false;
 			obst[y][x] = false;
 		}
+	}
 
 	steigen = 1, neu = 0, z = 0;
 	tb_clear();
 
+	//Set playerpositions
 	playerpos[player_y][player_x] = true;
 	playerpos[player_y][player_x+2] = true;
         playerpos[player_y-1][player_x+1] = true;
@@ -180,44 +199,49 @@ static void re_init (void)
        tb_put_cell(player_x+2, player_y-2, &player);
        tb_put_cell(player_x, player_y-2, &player);
 
+	t1 = time(NULL);
+
 	// Show output
 	tb_present();
 }
-
-
-
-
+//Move left, right and jump
 static int handle_key(uint16_t key)
 {
 	switch (key) {
 	case TB_KEY_ARROW_LEFT:		
 		for (y = 0; y < GAME_Y; y++)
 		{
-			if (playerpos[y][0] == true)
+			if (playerpos[y][1] == true)
 				;
 			else
 			{
-			for (x = 1; x < GAME_X - 1; x++)
-				for (y = 0; y < GAME_Y; y++)
-					if (playerpos[y][x])
+				for (x = 1; x < GAME_X - 1; x++)
+				{
+					for (y = 0; y < GAME_Y; y++)
 					{
-						playerpos[y][x] = false;
-						playerpos[y][x-1] = true;	
-						tb_put_cell(x, y, &empty);
-						tb_put_cell(x-1, y, &player);
+						if (playerpos[y][x])
+						{
+							playerpos[y][x] = false;
+							playerpos[y][x-1] = true;	
+							tb_put_cell(x, y, &empty);
+							tb_put_cell(x-1, y, &player);
+						}
 					}
+				}
 			}
 		}
 		break;
 	case TB_KEY_ARROW_RIGHT:
 		for (y = 0; y < GAME_Y; y++)
 		{
-			if (playerpos[y][GAME_X-1] == true)
+			if (playerpos[y][GAME_X-2] == true)
 				;
 			else
 			{
 				for (x = GAME_X - 2; x > 0; x--)
-					for (y = 0; y < GAME_Y; y++)
+			       	{
+					for (y = 0; y < GAME_Y; y++) 
+					{
 						if (playerpos[y][x])
 						{
 							playerpos[y][x] = false;
@@ -225,27 +249,15 @@ static int handle_key(uint16_t key)
 							tb_put_cell(x, y, &empty);
 							tb_put_cell(x+1, y, &player);
 						}
+					}
+				}
 			}
 		}
 
 		break;
 	case TB_KEY_SPACE:
-
 		if (z == 0)
 		       	z = 1;
-		/*
-		for (int i = 0; i < GAME_X; i++)
-		{
-			if (playerpos[GAME_Y-3][i])
-			{
-				playerpos[player_y][player_x] = false;
-				tb_put_cell(player_x, player_y, &empty);
-				player_y--;
-				playerpos[player_y][player_x] = true;
-				tb_put_cell(player_x, player_y, &player);
-			}
-		}
-		*/
 		break;
 	case TB_KEY_ESC:
 		return 1;
@@ -258,6 +270,47 @@ static int handle_key(uint16_t key)
 	}
 	return 0;
 }
+
+//Check if any truthvalues are overlapping, if yes stop game and let user decide whether to continue or to leave
+static int game_over (void)
+{
+	for (y = 0; y < GAME_Y; y++)
+	{
+		for (x = 0; x < GAME_X; x++)
+		{
+			if (playerpos[y][x] && obst[y][x])
+			{
+				tb_print (30, 28, "GAME OVER!");
+				tb_present();
+				sleep(2);
+				tb_print (25, 32, "Press ENTER to continue or ESC to leave"); 
+				tb_present();
+
+				do {
+					tb_poll_event(&leave);
+					if (leave.type == TB_EVENT_KEY)
+					{
+						answer = handle_key(leave.key);
+						if (answer == 2) 
+						{		
+							re_init();
+							return 0;
+						}			
+						if (answer == 1) 
+						{
+							return 1;
+						}
+					}
+				} while (true);
+
+			}
+		}
+	}
+	return 2;
+}
+
+
+
 
 int main(void)
 {
@@ -304,56 +357,49 @@ int main(void)
 			goto shutdown_tb;
 		}
 	}
-
-	struct tb_event input, leave;
-	clock_t timeanf;
-	double timediff, random, counter1 = 0, counter2 = 0;
-
+	//initialize board
+	
 	re_init();
+	t1 = time(NULL);
 
-    time_t t1 = time(NULL);
+
 	// Game loop
-
 
 	while (true) { // give user 10ms for input
 		timeanf = clock();
-		
-        // Uhrzeit block
-        char kuchen[20];
-        time_t t2 = time(NULL);
-        sprintf(kuchen,"Spielzeit %li:%li", (t2-t1)/60,(t2-t1)%60 );
-        tb_print(10,10,kuchen);
-        // Uhrzeit block ende
+
+		// Uhrzeit block
+		char kuchen[20];
+		t2 = time(NULL);
+		sprintf(kuchen,"Spielzeit %li:%li", (t2-t1)/60,(t2-t1)%60);
+		tb_print(10,10,kuchen);
+		// Uhrzeit block ende
 
 		tb_peek_event(&input, 10);
-		switch (input.type) {
-		case TB_EVENT_KEY:
+
+		if (input.type == TB_EVENT_KEY)
+	       	{
 			if (handle_key(input.key) == 1) goto shutdown_tb;
-			break;
-		default:
-			;
 		}
 		
-		
 		srand(time(NULL));
-		random = (double)((rand() % 2)+ 1)/100;
+		random = (double)((rand() % 3)+ 1)/130;
 
 		timediff = (double)(clock() - timeanf)/CLOCKS_PER_SEC;
-
 		counter1 += timediff;
 		counter2 += timediff;
 
-
 		if (counter1 > random)
-		{                                                    // (((rand() % 2)+ 1)/100)
-
-
-			obst[GAME_Y - 2][GAME_X-1] = true; 		
+		{                                                  
+			obst[GAME_Y - 2][GAME_X-2] = true; 		
 			obst[GAME_Y - 1][GAME_X-1] = true; 	//hindernis erscheint am rechten rand
-			counter1 = 0;
+			obst[GAME_Y - 1][GAME_X-2] = true;
+			obst[GAME_Y - 1][GAME_X-3] = true;
+
+		        counter1 = 0;
 		}
 
-		if (counter2 > 0.001) { 
+		if (counter2 > 0.0007) { 
 			update_obst();
 			if (steigen)
                 		steigen = update_jump();
@@ -365,36 +411,10 @@ int main(void)
 
 		tb_present();
 
-
-		for (y = 0; y < GAME_Y; y++) {
-			for (x = 0; x < GAME_X; x++) {
-				if (playerpos[y][x] && obst[y][x]) {
-                    			tb_print (30, 28, "GAME OVER!");
-					tb_present();
-					sleep(3);
-					tb_print (25, 32, "Press ENTER to continue or ESC to leave"); 
-					tb_present();
-
-					do {
-						tb_poll_event(&leave);
-						if (leave.type == TB_EVENT_KEY) {
-							answer = handle_key(leave.key);
-							if (answer == 2) 
-							{		
-								re_init();
-								neu = 1;
-
-							}			
-							if (answer == 1)  {
-								goto shutdown_tb;
-							}
-						}
-					} while (neu == 0);
-
-				}
-			}
-		}
-
+		//Game over after collision
+		int shutdown = game_over();
+		if (shutdown == 1)
+		       goto shutdown_tb;	
 	}
 
 shutdown_tb:
